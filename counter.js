f48d3d6c7b262a3fce592e4fc5170bd233df1a07
Michael Seth <michael.a.seth175@gmail.com>
2021-03-04 14:01:57 -0500

    added code to parse the Alice and Wonderlnad txt

--- counter.js
+++ counter.js
@@ -12,15 +12,19 @@ function greenEggs(para){
 
   let refinedText = "";
   let holder = "";
-  let noDoubleSpace = false;
-  let doubleSpaceCounter = 0;
 
   for (let t = 0; t < para.length; t++){
     if(para[t] == "\n") {
       refinedText += `${holder} `;
       holder = "";
     } else if (para[t] == "."){
-      refinedText += `${holder} `;
+      refinedText += `${holder}`;
+      holder = "";
+    } else if (para[t] == "!"){
+      refinedText += `${holder}`;
+      holder = "";
+    } else if (para[t] == "?"){
+      refinedText += `${holder}`;
       holder = "";
     } else if (para[t] == " ") {
       refinedText += `${holder} `;
@@ -34,8 +38,18 @@ function greenEggs(para){
       holder += para[t];
     }      
   }
-
   console.log(refinedText);
+
+  function noEmptyStrings (para) {
+    let holder = ""; 
+    for (let i = 0; i < refinedText.length; i++){
+      if(para[i] == !""){
+        holder += para[i];
+      }
+    } 
+  }
+ 
+
 
   let splitRefinedText = refinedText.split(' ');
   console.log(splitRefinedText);
@@ -50,3 +64,63 @@ function greenEggs(para){
   };
   console.log(textArray);
 }
+
+//Alice In WonderLand
+
+// var xhr2 = new XMLHttpRequest ();
+//     xhr2.onreadystatechange = function() {
+//       if (xhr2.readyState === 4 && xhr2.status == 200){
+//         document.getElementById("alice").innerHTML = xhr2.responseText;
+//         wonderLand(xhr2.responseText);
+//       }
+//     }
+//     xhr2.open("GET", "https://class-demo-files-and-resources.s3.amazonaws.com/Alice-In-Wonderland.txt");
+//     xhr2.send();
+
+// function wonderLand(para){
+
+//   let refinedText = "";
+//   let holder = "";
+//   let noDoubleSpace = false;
+
+//   for (let t = 0; t < para.length; t++){
+//     if(para[t] == "\n") {
+//       refinedText += `${holder} `;
+//       holder = "";
+//     } else if (para[t] == "."){
+//       refinedText += `${holder}`;
+//       holder = "";
+//     } else if (para[t] == "!"){
+//       refinedText += `${holder}`;
+//       holder = "";
+//     } else if (para[t] == "?"){
+//       refinedText += `${holder}`;
+//       holder = "";
+//     } else if (para[t] == " ") {
+//       refinedText += `${holder} `;
+//       holder = "";
+//     } else if (para[t] == ",") {
+//       refinedText += `${holder} `;
+//       holder = "";
+//     } else if (para[t] == para.length - 1) {
+//       refinedText += `${holder}`;
+//     } else {
+//       holder += para[t];
+//     }      
+//   }
+
+//   console.log(refinedText);
+
+//   let splitRefinedText = refinedText.split(' ');
+//   console.log(splitRefinedText);
+
+//   let textArray = {};
+//   for(let j = 0; j < splitRefinedText.length; j++){
+//     if (!textArray[splitRefinedText[j]]) {
+//       textArray[splitRefinedText[j]] = 1;
+//     } else {
+//       textArray[splitRefinedText[j]]++;
+//     }
+//   };
+//   console.log(textArray);
+// }
